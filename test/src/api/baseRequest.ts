8c67a37292f5024c888b21930f2e20dcf60f0a8e
Granevskiy8 <85808309+Granevskiy8@users.noreply.github.com>
2022-02-28 17:07:17 +0200

    Delete test directory

--- test/src/api/baseRequest.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const BaseUrl = "http://dev.trainee.dex-it.ru";
-
-export const BaseRequest = axios.create({
-  baseURL: "http://dev.trainee.dex-it.ru/api/",
-});
